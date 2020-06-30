39cf4a08f5ffb44f2129ccc107a87706f16ebc95
Alyson Souza <alysonforever@gmail.com>
2020-06-30 16:47:53 -0300

    merge tests

--- src/domain/data/usercases/local-save-purchases.spec.ts
+++ src/domain/data/usercases/local-save-purchases.spec.ts
@@ -42,10 +42,7 @@ describe('LocalSavePurchases', () => {
     const { sut, cacheStore } = makeSut()
     await sut.save()
     expect(cacheStore.deleteCallsCount).toBe(1)
-  });
-  test('Sould call delete with corret key', async () => {
-    const { sut, cacheStore } = makeSut()
-    await sut.save()
     expect(cacheStore.key).toBe('purchases')
   });
+
 });
